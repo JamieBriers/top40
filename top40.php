d36d8929b8df5ac976a82f9cdb7e73cbe6e29969
Chris Pomfret <cpom40ba@gmail.com>
2014-09-30 21:30:49 +0100

    Initial commit with old (not working top40 code)

---
+++ top40.php
@@ -0,0 +1,67 @@
+<?php
+// This is used to add a torrent file to rTorrent
+// Idea being, we use oldskoolscouse profile on TPB and pull new fastpiratebaytorrents from it every week.
+// Set cron job for '* 20 * * 0'	(Every Sunday at 8)
+// Chris Pomfret - Sept 2014
+// or /usr/local/bin/php
+$profileURL = "http://pirateproxy.bz/user/oldskoolscouse/0/3/0";
+$data;	// Global for returning feed data
+$filename = "top40_data";
+$lastDownloadedTime;	// Time the last torrent we downloaded was released
+
+getFeed($profileURL);	// First we load the feed (rss info now in global $data)
+loadData();					// Load $lastDownloadedTime from $filename
+
+$xml = new SimpleXMLElement($data);
+
+$xml.toString();
+exit();
+
+$publishTime = strtotime($xml->html->bodychannel->item[0]->pubDate);		// Publish date of latest torrent - Convert real format to UNIX time, much easier to work with
+
+// Now we work out it out if we need to download
+if ($publishTime > $lastDownloadedTime){
+	// A new torrent has been released
+	
+	// Update the last downloaded time to this torrent
+	$lastDownloadedTime = $publishTime;	
+	
+	// Save last downloaded time to file.
+	$fh = fopen($filename, 'w') or die("can't open file");
+	fwrite($fh, $lastDownloadedTime);
+	fclose($fh);
+
+	
+	
+	// Now we need to send the found URI to rTorrent.
+	echo shell_exec("transmission-remote -a ".$xml->channel->item[0]->torrent->magnetURI);
+}
+else{
+	echo "Nothing New";
+	// Nothing new, wait for another execution
+}
+
+// Loads last downloaded torrent time
+function loadData(){
+	global $filename, $lastDownloadedTime;
+	if (file_exists($filename)){
+		//File exists
+		$fileHandle = fopen($filename, 'r') or die("can't open file");
+		$lastDownloadedTime = fread($fileHandle, filesize($filename));
+		fclose($fileHandle);
+	}else{
+		echo ("I think not");
+	}
+}
+
+// Used to grab feed data
+function getFeed($feedURI){
+	global $data;
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $feedURI);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$data = curl_exec($ch);
+	curl_close($ch);
+}
+?>
